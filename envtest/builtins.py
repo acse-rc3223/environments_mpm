bc73e4356a01de9002c33926979601a6e1d04015
acse-rc3223 <roryjcs@gmail.com>
2023-10-09 01:40:44 +0100

    Added pandas script.

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,8 +1,9 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
 from scipy import misc
+import pandas as pd
 
-__all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve', 'create_pandas_series']
 
 
 def rand_array(shape):
@@ -13,3 +14,8 @@ def smooth_image(a, sigma=1):
 
 def my_mat_solve(A, b):
     return A.inv()*b
+
+def create_pandas_series(list):
+    return pd.Series([list])
+
+
---
+++ scripts/create_pandas_series.py
@@ -0,0 +1,5 @@
+from envtest import create_pandas_series
+
+x = create_pandas_series([1,2,3,4])
+
+print(x)
